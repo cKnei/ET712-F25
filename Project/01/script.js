f48df454311143ade6592ac4f9b8b37c43a158b5
Knei <76569087+cKnei@users.noreply.github.com>
2025-10-28 15:30:23 -0400

    In class

--- Project/01/script.js
+++ Project/01/script.js
@@ -1,5 +1,40 @@
 /**
- * Knei | Lab 
- *
+ * Knei | Project 1
  */
+
+/**
+ * ========================================
+ * CONSTANT VARIABLE DECLARATION
+ * ========================================
+ */
+const SECTION = {
+    clothing:   document.querySelector("#clothing"),
+    colours:    document.querySelector("#colours"),
+    numbers:    document.querySelector("#numbers"),
+    shapes:     document.querySelector("#shapes"),
+}
+
+/**
+ * ========================================
+ * FUNCTION DECLARATION
+ * ========================================
+ */
+function toggleSection(section) {
+    for ( let s in SECTION ) {
+        SECTION[s].classList.add("hidden");
+    }
+
+    SECTION[section].classList.remove("hidden");
+}
+/**
+ * ========================================
+ * PAGE INITIALIZATION
+ * ========================================
+ */
+for ( let navigation_option of document.querySelectorAll(".navigation_option") ) {
+    navigation_option.addEventListener("click", function(event) {
+        event.preventDefault();
+        toggleSection(navigation_option.textContent.toLowerCase());
+    });
+}
 
